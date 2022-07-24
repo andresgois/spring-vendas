dbfd613e3457e8820b1fb30d51cef9a6b2c2b557
Andre Gois <andresousalee@gmail.com>
2022-07-24 00:24:08 -0300

    :white_check_mark: test: rest controller hello

--- src/main/java/br/com/vendas/Application.java
+++ src/main/java/br/com/vendas/Application.java
@@ -2,10 +2,18 @@ package br.com.vendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
+	@GetMapping("/")
+	public String helloWorld() {
+		return "Hello world";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
